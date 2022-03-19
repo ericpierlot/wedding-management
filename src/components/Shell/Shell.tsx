f4311acd6deb6ff5ff4d4aec8c3136a0550809e9
Eric <Eric.p.83@gmail.com>
2022-03-19 18:30:49 +0100

    add total price in the header

--- src/components/Shell/Shell.tsx
+++ src/components/Shell/Shell.tsx
@@ -54,6 +54,9 @@ const HeaderApp = () => {
         {nbGuests} {nbGuests && nbGuests > 1 ? "guests" : "guest"}
       </Text>
       <div>
+        <Text component="span" weight="bold">
+          {formattedNumber(total)}
+        </Text>
         <Text
           component="span"
           weight="bold"
